369550b240e3b294cc7a88b1c8f2de1b5dc32c40
Joan Gladbach <jjgr29@umsystem.edu>
2025-01-08 14:21:49 -0600

    Update Spr25

--- CS201L-Lab7/Pet.cpp
+++ CS201L-Lab7/Pet.cpp
@@ -1,2 +1,51 @@
 #include "Pet.h"
- 
+
+
+// Function to parse a date string in the format mm/dd/yyyy
+void parseDate(const std::string& date, int& month, int& day, int& year) {
+    char delimiter;
+    istringstream iss(date);
+    iss >> month >> delimiter >> day >> delimiter >> year;
+}
+
+// Function to convert a date to Julian day number
+int dateToJulian(int month, int day, int year) {
+    if (month <= 2) {
+        year--;
+        month += 12;
+    }
+    int A = year / 100;
+    int B = 2 - A + (A / 4);
+    int JD = (365.25 * (year + 4716)) + (30.6001 * (month + 1)) + day + B - 1524.5;
+    return static_cast<int>(JD);
+}
+/*
+
+// This is sample code to calculate the current age
+
+void Pet::setAgeCurrent() {
+    int m1, d1, y1;
+    int m2, d2, y2;
+
+    parseDate(birthDate, m1, d1, y1);
+
+    struct tm timeNow;
+    time_t now = time(0);
+    localtime_s(&timeNow, &now);
+
+    y2 = timeNow.tm_year + 1900;
+    m2 = timeNow.tm_mon + 1;
+    d2 = timeNow.tm_mday;
+
+    int jd1 = dateToJulian(m1, d1, y1);
+    int jd2 = dateToJulian(m2, d2, y2);
+
+    int daysDifference = jd2 - jd1;
+
+    double yearsDifference = daysDifference / 365.25; // Convert days to years, accounting for leap years
+
+    ageCurrent = yearsDifference;
+
+
+}
+*/
--- CS201L-Lab7/Pet.h
+++ CS201L-Lab7/Pet.h
@@ -1,4 +1,25 @@
 #pragma once 
+#include <time.h>
+#include <string>
+#include <sstream>
+#include <iomanip>
+#include <iostream>
+
+using namespace std;
 
 class Pet {
+private:
+	string ID;
+	string name;
+	string dateTime;
+	string birthDate;
+	double ageAtDateTime;  //NOTE: this is calculated
+	double ageCurrent;	   //NOTE: this is calculated
+	string outcomeType;
+	string type;
+	string sex;
+	string breed;
+
+public:
+	//getters & setters
 };
